2174fa51ff8761353c9de79c3fb8099bf109e384
Shawn <160689002+xx-Shinei-xx@users.noreply.github.com>
2024-04-14 23:48:11 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,18 +78,16 @@ if info_button_state:
     # Create expandable sections for different parts of the summary
     with st.expander('Marco Teórico'):
         st.write("""
-        El COVID-19, derivado de "Coronavirus Disease 2019", es una afección respiratoria provocada por el virus SARS-CoV-2,
-        perteneciente a la familia de los coronavirus, que también incluye el SARS-CoV y el MERS-CoV. La principal vía de
-        transmisión del SARS-CoV-2 es de persona a persona a través de gotas respiratorias expulsadas al toser, estornudar,
-        hablar o respirar, aunque también puede propagarse por contacto con superficies u objetos contaminados con el virus
-        y luego tocarse la boca, la nariz o los ojos.
+        El COVID-19, derivado de "Coronavirus Disease 2019", es una afección respiratoria provocada por el virus SARS-CoV-2, perteneciente a la familia de los coronavirus, que también incluye el SARS-CoV y el MERS-CoV. La principal vía de transmisión del SARS-CoV-2 es de persona a persona a través de gotas respiratorias expulsadas al toser, estornudar, hablar o respirar, aunque también puede propagarse por contacto con superficies u objetos contaminados con el virus y luego tocarse la boca, la nariz o los ojos.
 
-        En Guatemala, al igual que en muchos otros países, el COVID-19 ha tenido un impacto significativo en la salud pública y
-        en la sociedad en general. Desde el inicio de la pandemia, el país ha experimentado un número considerable de casos
-        confirmados de COVID-19, detectados mediante pruebas de laboratorio como PCR y pruebas rápidas de antígenos. La
-        transmisión comunitaria del virus ha sido especialmente desafiante en áreas urbanas densamente pobladas de Guatemala,
-        llevando a un aumento en la carga de casos en hospitales y centros de salud, poniendo a prueba la capacidad del sistema
-        de salud guatemalteco.
+En Guatemala, al igual que en muchos otros países, el COVID-19 ha tenido un impacto significativo en la salud pública y en la sociedad en general. Desde el inicio de la pandemia, el país ha experimentado un número considerable de casos confirmados de COVID-19, detectados mediante pruebas de laboratorio como PCR y pruebas rápidas de antígenos. La transmisión comunitaria del virus ha sido especialmente desafiante en áreas urbanas densamente pobladas de Guatemala, llevando a un aumento en la carga de casos en hospitales y centros de salud, poniendo a prueba la capacidad del sistema de salud guatemalteco.
+
+El gobierno guatemalteco ha implementado diversas medidas para prevenir la propagación del virus, incluyendo cuarentenas, toques de queda y el uso obligatorio de mascarillas. Asimismo, se han llevado a cabo campañas de vacunación masiva como parte de las estrategias de control. La disponibilidad de camas de hospital, equipos médicos y personal sanitario ha sido un problema en algunos momentos de la pandemia, lo que ha puesto a prueba la capacidad del sistema de salud guatemalteco.
+
+La pandemia ha exacerbado los desafíos socioeconómicos en Guatemala, con muchas personas perdiendo sus empleos o enfrentando dificultades económicas debido a las restricciones implementadas para contener el virus. Esto ha afectado especialmente a comunidades vulnerables y sectores informales de la economía. La vacunación contra el COVID-19 ha sido una prioridad en Guatemala, al igual que en todo el mundo, buscando proteger a la población contra la enfermedad y reducir la gravedad de los casos.
+
+La respuesta a la pandemia en Guatemala ha sido multifacética, involucrando no solo medidas de salud pública, sino también esfuerzos para abordar los impactos socioeconómicos y garantizar el acceso equitativo a la atención médica y a las vacunas. La colaboración entre el gobierno, las organizaciones de salud y la sociedad civil ha sido fundamental para enfrentar los desafíos planteados por el COVID-19 y trabajar hacia la recuperación integral del país. La investigación científica ha sido clave para entender la enfermedad y desarrollar vacunas y tratamientos eficaces.
+
         """)
 
     with st.expander('Objetivos Generales'):
