5e6f02e7518cd41c017d12c8efaf8abb35ba5344
Shawn <160689002+xx-Shinei-xx@users.noreply.github.com>
2024-04-22 22:16:50 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,144 +1,67 @@
 import pandas as pd
-import streamlit as st
-import plotly.graph_objects as go
 import numpy as np
+import streamlit as st
+from scipy.stats import poisson, norm, chi2_contingency
+import matplotlib.pyplot as plt
 
+# Cargar datos desde el archivo CSV en el mismo directorio
+data1 = pd.read_csv('data1.csv')
 
-# Definir la distribucion de gauss
-def gaussian(x, A, u, r):
-    return A * np.exp(-((x - u) / r) ** 2 / 2)
+# Identificar el nombre correcto de la columna para las mediciones
+columna_mediciones = "decaimiento solo con el aire"  # Ajustar según el nombre real de la columna
 
-# definir la funcion de gauss
-def plot_covid_data(data, A, u, r):
-    fig = go.Figure()
-    x_fit = np.linspace(0, len(data) - 1, 100)
+# Extraer las mediciones del DataFrame
+mediciones = data1[columna_mediciones]
 
-    # plotear
-    fig.add_trace(go.Bar(
-        x=data.index,
-        y=data['Casos por fecha de emisión de resultados'],
-        name='Días VS total de contagios',
-        marker=dict(color=data['Casos por fecha de emisión de resultados'], colorscale='Reds')
-    ))
+# Definir la aplicación Streamlit
+def main():
+    st.title('Ajuste de Distribución y Prueba χ²')
 
-    # fit de la distribucion de gauss
-    fit_y = gaussian(x_fit, A, u, r)
-    fig.add_trace(go.Scatter(
-        x=x_fit,
-        y=fit_y,
-        name='Fit',
-        mode='lines'
-    ))
-
-    # tectos en las graficas
-    fig.update_layout(
-        title='Gráfico combinado - Conjunto de datos COVID-19',
-        xaxis_title='Número de día',
-        yaxis_title='Casos por fecha de emisión de resultados',
-        barmode='group'
-    )
-
-    return fig
-
-# cargar datos 1
-data1 = pd.read_csv('datos1.csv')
-A1 = 465.464
-u1 = 89.2538
-r1 = 7.13597
-
-# cargar datos 2
-data2 = pd.read_csv('datos2.csv')
-A2 = 1062.39
-u2 = 139.803
-r2 = 35.1769
-
-#  Streamlit
-st.title('Visualización Interactiva de Datos COVID-19')
-
-#adornos para la grafica 1
-st.sidebar.title('Parámetros de Ajuste Gaussiano')
-st.sidebar.subheader('Primer conjunto de datos')
-A_slider1 = st.sidebar.slider('Amplitud', min_value=0.0, max_value=1000.0, value=A1, step=10.0)
-u_slider1 = st.sidebar.slider('Media', min_value=0.0, max_value=100.0, value=u1, step=1.0)
-r_slider1 = st.sidebar.slider('Desviación estándar', min_value=0.0, max_value=20.0, value=r1, step=0.1)
-
-# adornos para la grafica 2
-st.sidebar.subheader('Segundo conjunto de datos')
-A_slider2 = st.sidebar.slider('Amplitud', min_value=0.0, max_value=2000.0, value=A2, step=10.0)
-u_slider2 = st.sidebar.slider('Media', min_value=0.0, max_value=200.0, value=u2, step=1.0)
-r_slider2 = st.sidebar.slider('Desviación estándar', min_value=0.0, max_value=40.0, value=r2, step=0.1)
-
-# mostrar
-info_button_state = st.button('Mostrar Información sobre COVID-19')
-
-if info_button_state:
-    st.subheader('Laboratorio 2: Predicción de COVID19')
-
-    # para el reporte
-    with st.expander('Resumen'):
-        st.write("""
-En respuesta a la pandemia de COVID-19 en Guatemala, se utilizan técnicas estadísticas para modelar y prever la evolución de casos hasta una fecha específica, mediante el ajuste de una distribución binomial a datos del Ministerio de Salud. Este enfoque permite anticipar tendencias futuras y respaldar la toma de decisiones en salud pública.        
-                """)
+    # Mostrar histograma de las mediciones con parámetros interactivos
+    st.subheader('Histograma de Mediciones')
     
-    with st.expander('Marco Teórico'):
-        st.write("""
-        El COVID-19, derivado de "Coronavirus Disease 2019", es una afección respiratoria provocada por el virus SARS-CoV-2, perteneciente a la familia de los coronavirus, que también incluye el SARS-CoV y el MERS-CoV. La principal vía de transmisión del SARS-CoV-2 es de persona a persona a través de gotas respiratorias expulsadas al toser, estornudar, hablar o respirar, aunque también puede propagarse por contacto con superficies u objetos contaminados con el virus y luego tocarse la boca, la nariz o los ojos.
+    # Parámetros interactivos del histograma
+    num_bins = st.slider('Número de Bins:', min_value=5, max_value=50, value=20)
+    range_min = st.slider('Valor Mínimo:', min_value=int(mediciones.min()), max_value=int(mediciones.max()), value=int(mediciones.min()))
+    range_max = st.slider('Valor Máximo:', min_value=int(mediciones.min()), max_value=int(mediciones.max()), value=int(mediciones.max()))
 
-En Guatemala, al igual que en muchos otros países, el COVID-19 ha tenido un impacto significativo en la salud pública y en la sociedad en general. Desde el inicio de la pandemia, el país ha experimentado un número considerable de casos confirmados de COVID-19, detectados mediante pruebas de laboratorio como PCR y pruebas rápidas de antígenos. La transmisión comunitaria del virus ha sido especialmente desafiante en áreas urbanas densamente pobladas de Guatemala, llevando a un aumento en la carga de casos en hospitales y centros de salud, poniendo a prueba la capacidad del sistema de salud guatemalteco.
+    # Crear el histograma con los parámetros interactivos
+    plt.hist(mediciones, bins=num_bins, range=(range_min, range_max), color='skyblue', edgecolor='black', alpha=0.7, label='Mediciones')
+    plt.xlabel('Mediciones')
+    plt.ylabel('Frecuencia')
+    plt.title('Histograma Personalizable')
 
-El gobierno guatemalteco ha implementado diversas medidas para prevenir la propagación del virus, incluyendo cuarentenas, toques de queda y el uso obligatorio de mascarillas. Asimismo, se han llevado a cabo campañas de vacunación masiva como parte de las estrategias de control. La disponibilidad de camas de hospital, equipos médicos y personal sanitario ha sido un problema en algunos momentos de la pandemia, lo que ha puesto a prueba la capacidad del sistema de salud guatemalteco.
+    # Ajuste de la distribución seleccionada sobre el histograma
+    tipo_distribucion = st.radio("Seleccionar Distribución:", ('Poisson', 'Gaussiana'))
+    x_fit = np.linspace(range_min, range_max, 100)
 
-La pandemia ha exacerbado los desafíos socioeconómicos en Guatemala, con muchas personas perdiendo sus empleos o enfrentando dificultades económicas debido a las restricciones implementadas para contener el virus. Esto ha afectado especialmente a comunidades vulnerables y sectores informales de la economía. La vacunación contra el COVID-19 ha sido una prioridad en Guatemala, al igual que en todo el mundo, buscando proteger a la población contra la enfermedad y reducir la gravedad de los casos.
+    if tipo_distribucion == 'Poisson':
+        # Ajuste de distribución de Poisson
+        lambda_poisson = st.slider('λ (parámetro Poisson):', min_value=0.1, max_value=10.0, value=1.0, step=0.1)
+        y_fit = poisson.pmf(x_fit, lambda_poisson) * len(mediciones)
+        plt.plot(x_fit, y_fit, color='red', label=f'Ajuste Poisson (λ={lambda_poisson})')
 
-La respuesta a la pandemia en Guatemala ha sido multifacética, involucrando no solo medidas de salud pública, sino también esfuerzos para abordar los impactos socioeconómicos y garantizar el acceso equitativo a la atención médica y a las vacunas. La colaboración entre el gobierno, las organizaciones de salud y la sociedad civil ha sido fundamental para enfrentar los desafíos planteados por el COVID-19 y trabajar hacia la recuperación integral del país. La investigación científica ha sido clave para entender la enfermedad y desarrollar vacunas y tratamientos eficaces.
+    elif tipo_distribucion == 'Gaussiana':
+        # Ajuste de distribución Gaussiana
+        media = st.slider('Media:', min_value=0.0, max_value=10.0, value=5.0, step=0.1)
+        desviacion_estandar = st.slider('Desviación Estándar:', min_value=0.1, max_value=5.0, value=1.0, step=0.1)
+        y_fit = norm.pdf(x_fit, media, desviacion_estandar) * len(mediciones)
+        plt.plot(x_fit, y_fit, color='red', label=f'Ajuste Gaussiano (Media={media}, Desv. Estándar={desviacion_estandar})')
 
-        """)
- 
-    
-    with st.expander('Objetivos Generales'):
-        st.write("""
-        Aplicar técnicas estadísticas para modelar la evolución de casos de COVID-19 en Guatemala hasta una fecha en específico utilizando datos del Ministerio de Salud, mediante el ajuste de una distribución binomial y la realización de predicciones.
-        """)
+    # Mostrar leyenda y configurar diseño
+    plt.legend()
+    plt.grid(True)
+    st.pyplot()
 
-    with st.expander('Objetivos Específicos'):
-        st.write("""
-        - Recolectar datos históricos: Obtener datos precisos de casos de COVID-19 del Ministerio de Salud de Guatemala.
-        - Modelar con distribución binomial: Representar el número de nuevos casos diarios como una distribución binomial.
-        - Hacer predicciones futuras: Utilizar el modelo ajustado para prever la cantidad esperada de nuevos casos después del 15 de marzo de 2021.
-        """)
+    # Realizar la prueba χ² de contingencia
+    frecuencias_observadas, _ = np.histogram(mediciones, bins=num_bins, range=(range_min, range_max))
+    valores_esperados = y_fit
+    chi2, valor_p = chi2_contingency(np.array([frecuencias_observadas, valores_esperados]).T)
 
-    with st.expander('Conclusiones'):
-        st.write("""
-        - Durante el primer intervalo de tiempo, se observó una adecuada representación de los datos mediante una función gaussiana, lo que resultó en una predicción precisa. Sin embargo, en el segundo intervalo, con una mayor cantidad de datos, la precisión del ajuste disminuyó en comparación con el primero. Aunque la predicción en este intervalo no alcanzó el nivel de certeza del primer ajuste, aún mostró un rendimiento aceptable, como se evidencia en la gráfica.
-        - El ajuste de una distribución binomial permitió estimar la probabilidad diaria de nuevos casos y el número total de días en el modelo, brindando información valiosa para las predicciones.
-        - Las predicciones ofrecen una visión proyectiva de la evolución esperada de la pandemia, facilitando la planificación de intervenciones futuras.
-        - La vigilancia epidemiológica continua y la recopilación de datos precisos son esenciales para mejorar la precisión de los modelos y respaldar decisiones basadas en evidencia en la gestión de la pandemia.
-        """)
-    with st.expander('Discusión de datos'):
-        st.write("""
-        Se puede apreciar una mayor precisión en la predicción del primer gráfico, gracias a un ajuste más certero. Sin embargo, en el segundo gráfico, a pesar de aplicar el mismo ajuste de una función gaussiana, la predicción parece aproximarse, pero no logra ajustarse completamente a los datos.      
-        """)
-    with st.expander('Referencias'):
-        st.write("""
-        - Taylor, John R. “An introduction to error analysis, The study of uncertainties in physical measurements”. Second edition. University science books. 1982.
-        - https://www.w3schools.com/python/numpy/numpy_random_normal.asp
-        - https://pro.arcgis.com/es/proapp/latest/help/analysis/geoprocessing/charts/histogram.html
-        """)
+    # Mostrar resultado de la prueba χ²
+    st.subheader('Resultado de la Prueba χ²')
+    st.write(f'Valor p de la prueba χ²: {valor_p:.4f}')
 
-   
-# botones 
-run_button_state = st.button('Ejecutar Código')
-
-if run_button_state:
-    st.write('**Visualización de Datos**')
-
-    # data 1
-    st.subheader('Primer conjunto de datos')
-    fig1 = plot_covid_data(data1, A_slider1, u_slider1, r_slider1)
-    st.plotly_chart(fig1)
-
-    #  data 2
-    st.subheader('Segundo conjunto de datos')
-    fig2 = plot_covid_data(data2, A_slider2, u_slider2, r_slider2)
-    st.plotly_chart(fig2)
-
+# Ejecutar la aplicación
+if __name__ == '__main__':
+    main()
